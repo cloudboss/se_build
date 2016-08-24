33c2a7bcadd336befee64cea6a0658361aac8332
Joseph Wright <rjosephwright@gmail.com>
2016-08-23 23:17:02 -0500

    Add version argument to build script

--- build.sh
+++ build.sh
@@ -1,16 +1,18 @@
-#!/bin/sh
+#!/bin/sh -e
 
 app=${1}
+version=${2}
 
 usage()
 {
-    echo "Usage: ${0} <app>"
+    echo "Usage: ${0} <app> <version>"
     exit ${1}
 }
 
-[ ${#} = 1 ] || usage 1
+[ ${#} = 2 ] || usage 1
 
-set -xe && \
-  git clone /repo /home/elixir/see_eye && \
-  cd /home/elixir/see_eye/apps/${app} && \
-  ./build.sh
+git clone /repo /home/elixir/see_eye
+cd /home/elixir/see_eye
+mix deps.get
+mix release --profile ${app}:prod
+docker build -t ${app}:${version} rel/${app}/releases/${version}
